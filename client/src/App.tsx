1af23c610c25df84b305573347f6d7a86dad473f
shacharshemesh <shacharsms10@gmail.com>
2025-03-05 21:02:40 +0200

    init client

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import Home from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+
+  return (
+    <Router>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ backgroundColor: "#F9E4E3", height: "100vh" }}
+      >
+        (
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        )
+      </div>
+    </Router>
+  );
+}
+
+export default App;
